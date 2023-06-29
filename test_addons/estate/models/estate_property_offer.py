dfb97b41791fe5a673b4de82937a2873e5171c2d
RokasBarkevicius <rokasbarkevicius@gmail.com>
2023-06-29 16:07:41 +0300

    Chapter 9: date

--- test_addons/estate/models/estate_property_offer.py
+++ test_addons/estate/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
@@ -13,5 +13,14 @@ class EstatePropertyOffer(models.Model):
     )
     partner_id = fields.Many2one("res.partner", string = "Offeror",required=True)
     property_id = fields.Many2one("estate.property", string = "Property",required=True)
+    validity = fields.Integer(default = 7, string = "Validity (days)")
+    date_deadline = fields.Date(compute = "_compute_date_deadline", inverse = "_inverse_date_deadline")
 
-    
+    @api.depends("validity")
+    def _compute_date_deadline(self):
+        for record in self:
+            record.date_deadline = fields.Datetime.add(fields.Datetime.now(), days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            record.date_deadline = fields.Date()
